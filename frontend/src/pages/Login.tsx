ae885cb03ac2399c7b5c6cd9c25ee06c079510a7
omar Allaham <omarlaham246@gmail.com>
2026-03-14 01:44:11 +0300

    implement Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,29 +1,71 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+// import { loginUser } from "../api/jobsApi";
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // try {
+    //   await loginUser(formData);
+    //   navigate("/Dashboard");
+    // } catch (error) { console.error(error) }
+
+    if (formData.email && formData.password) {
+      navigate("/Dashboard");
+    }
+  };
+
   return (
-    <div>
-      <form>
+    <div className="login-wrapper">
+      <form onSubmit={handleSubmit}>
         <h2>Job Tracker</h2>
-        <p>Track you job applications</p>
-        <label>Email</label>
+        <p>Track your job applications</p>
+
+        <label htmlFor="email">Email</label>
         <input
+          id="email"
+          name="email" // يجب أن يطابق الاسم في الـ State
           type="email"
-          title="Email"
           placeholder="Your.email@example.com"
+          value={formData.email}
+          onChange={handleChange}
+          required
         />
-        <label>Password</label>
-        <input type="password" title="Password" placeholder="••••••••" />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="••••••••"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
         <button type="submit">Login</button>
+
         <p>Don't have an account?</p>
       </form>
-      <button
-        onClick={() => {
-          return navigate("./Register");
-        }}
-      >
-        Register here{" "}
+
+      <button type="button" onClick={() => navigate("/Register")}>
+        Register here
       </button>
     </div>
   );
